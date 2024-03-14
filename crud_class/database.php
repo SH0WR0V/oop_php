dd6e47a3582f2fe928b2c2befa1c0ce83c1d06ec
SH0WR0V <sm.shahriar1230@gmail.com>
2024-03-15 02:17:57 +0600

    delete method in class added

--- crud_class/database.php
+++ crud_class/database.php
@@ -70,8 +70,22 @@ class Database
         }
     }
 
-    public function delete()
+    public function delete($table, $where = null)
     {
+        if ($this->tableExists($table)) {
+            $sql = "DELETE FROM $table";
+            if ($where != null) {
+                $sql .= " WHERE $where";
+            }
+            if ($this->mysqli->query($sql)) {
+                array_push($this->result, $this->mysqli->affected_rows);
+            } else {
+                array_push($this->result, $this->mysqli->error);
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
     public function select()
--- crud_class/index.php
+++ crud_class/index.php
@@ -6,6 +6,10 @@ $obj = new Database();
 // echo "Insert result is: ";
 // print_r($obj->getResult());
 
-$obj->update('students', ['student_name' => 'Mahmud', 'age' => 17, 'city' => 'Dhaka'], 'id = "4"');
-echo "Update result is: ";
+// $obj->update('students', ['student_name' => 'Mahmud', 'age' => 17, 'city' => 'Dhaka'], 'id = "4"');
+// echo "Update result is: ";
+// print_r($obj->getResult());
+
+$obj->delete('students', 'id = "3"');
+echo "Delete result is: ";
 print_r($obj->getResult());
